2e49c1a922d68a039e41c7e033cbfb5d6ab0c23e
LinGaOscar <oscar987123@yahoo.com.tw>
2025-05-11 20:58:48 +0800

    Work with int long double and decimal

--- numbersharp/Program.cs
+++ numbersharp/Program.cs
@@ -1,4 +1,22 @@
 ﻿int a = 18;
-int b = 6;
+int b = 2100000000;
 int c = a + b;
-Console.WriteLine(c);
+Console.WriteLine($"C = {c}");
+//int 上限 2,147,483,647
+long d = a + b * c;
+Console.WriteLine($"D = {d}");
+
+long e = a + (long)b * c;
+Console.WriteLine($"E = {e}");
+
+e = ((long)a + b) * c;
+Console.WriteLine($"D = {e}");
+
+
+double aa = 1.0;
+double bb = 3.0;
+Console.WriteLine(aa / bb);
+
+decimal cc = 1.0M;
+decimal dd = 3.0M;
+Console.WriteLine(cc / dd);

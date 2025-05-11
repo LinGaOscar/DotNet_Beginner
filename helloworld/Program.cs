3be75e0a8f6bc29e44cd4b291df7bcdd15348c41
LinGaOscar <oscar987123@yahoo.com.tw>
2025-05-11 17:02:26 +0800

    Add String variable for console

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Oscar!");
+
+
+string firstFriend = "Bill";
+string secondFriend = "John";
+
+Console.WriteLine($"Hello, {firstFriend} and {secondFriend}!");

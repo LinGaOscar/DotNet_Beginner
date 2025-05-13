99c0ecd8f313a10314bdd4b9f3f9ff613f6360ba
LinGaOscar <oscar987123@yahoo.com.tw>
2025-05-13 23:40:07 +0800

    basic type

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int i = 0;
+short sh = 0;
+long lo = 0;
+Console.WriteLine("i = " + i);
+Console.WriteLine("i = {0}, {1}", i, "string");
+// ctrl + k + d 快速對齊
+double d = 0;
+double dl = 0d;
+float f = 0;
+float fl = 0f;
+decimal dm1 = 0;
+decimal dm2 = 0m;
+bool b = false;
+
+char c1 = 'A'; // ' 2byte
+
+string s1 = "this is a string";
+const string s2 = "this is a string";
+
+var d5 = 0d;
+var f5 = 0f;
+var dm5 = 5m;
+var cc = 'b';
+var s3 = "string";
